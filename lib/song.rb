554866f9a5df7ba6cfecabe65c09e5c1ffa77a81
Tyler Firth <tyfirth@gmail.com>
2020-05-28 01:20:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,11 @@ class Song
   end
   
   def self.create_from_filename(name)
-    
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << self
+    song
   end
   
   def self.destroy_all
