6062885a1da7839fd50059d557cca9786ab4ffa0
Tyler Firth <tyfirth@gmail.com>
2020-05-28 01:20:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    @@all << self
+    @@all << song
     song
   end
   
